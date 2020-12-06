f9df8587cb4a04b39c52e2c70d74b7508c5fc241
Oscar Rodriguez <oscar.rodriguez@icahn.mssm.edu>
2020-12-06 13:34:36 -0500

    add

--- bash/create_ref_index.sh
+++ bash/create_ref_index.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e -x
 
-ref_fastafn=$1
-outdir=$2
+ref_fastafn=$1 # Full path only
+outdir=$2 # Full path only
 
 samtools faidx ${ref_fastafn}
 
-for folder in minimap2 pbmm2 blasr
+for folder in minimap2_pb minimap2_ont pbmm2 blasr
 do
     mkdir -p ${outdir}/${folder}
     if [ ! -s ${outdir}/${folder}/ref.fa.fai ]
@@ -16,9 +16,9 @@ do
     fi
 done
 
-minimap2 -x map-ont -d ${outdir}/minimap2/ref.mmi ${outdir}/minimap2/ref.fa
-minimap2 -x map-pb -d ${outdir}/minimap2/ref.mmi ${outdir}/minimap2/ref.fa
-
-pbmm2 index ${outdir}/pbmm2/ref.fa ${outdir}/pbmm2/ref.mmi
+minimap2 -x map-ont -d ${outdir}/minimap2_ont/ref.mmi ${outdir}/minimap2_ont/ref.fa
+minimap2 -x map-pb -d ${outdir}/minimap2_pb/ref.mmi ${outdir}/minimap2_pb/ref.fa
 
 sawriter ${outdir}/blasr/ref.fa
+
+pbmm2 index ${outdir}/pbmm2/ref.fa ${outdir}/pbmm2/ref.mmi
